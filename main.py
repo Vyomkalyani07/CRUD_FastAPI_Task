9bc6c78c7c632c8b86fceda9bcc66a02d821ce9e
Kartikiscalm <kartikjayantai@gmail.com>
2026-06-14 15:09:19 +0530

    issue resolved

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI , Path
 import json
-from pydantic import BaseModel , computed_field , Field
+from pydantic import BaseModel, computed_field, Field, field_validator
 from typing import Optional
 from prometheus_fastapi_instrumentator import Instrumentator
 
@@ -11,27 +11,50 @@ Instrumentator().instrument(app).expose(app)
 
 class Patient(BaseModel):
     
-    id:str
-    name:str
-    Gender:str
-    age:int 
-    height:float=Field(description="in meters",example=1.67)
-    weight:float
-    
+    id: str
+    name: str
+    Gender: str
+    age: int
+    height: float = Field(description="in meters", example=1.67)
+    weight: float
+
+    @field_validator('age')
+    @classmethod
+    def validate_age(cls, v):
+        if v <= 0:
+            raise ValueError('Age must be greater than 0')
+        return v
+
+    @field_validator('weight')
+    @classmethod
+    def validate_weight(cls, v):
+        if v <= 0:
+            raise ValueError('Weight must be greater than 0')
+        return v
+
+    @field_validator('height')
+    @classmethod
+    def validate_and_convert_height(cls, v):
+        if v <= 0:
+            raise ValueError('Height must be greater than 0')
+        # cm ko meter mein convert kar dega, divide by 100 then roundof till 2 decimal places
+        if v > 3.0:
+            v = round(v / 100, 2)
+        return v
+
     @computed_field
     @property
     def bmi(self) -> float:
-        bmi=round(self.weight/(self.height**2) , 2)
-        return bmi
-    
+        return round(self.weight / (self.height ** 2), 2)
+
     @computed_field
     @property
     def verdict(self) -> str:
-        if self.bmi<18:
-            return "underwieght"
-        elif self.bmi<25:
-            return "fit"
-        elif self.bmi<30:
+        if self.bmi < 18.5:
+            return "Underweight"
+        elif self.bmi < 25:
+            return "Fit"
+        elif self.bmi < 30:
             return "Overweight"
         else:
             return "Obese"
@@ -129,5 +152,3 @@ def delet_patient_data(patient_id:str):
     save_data(data)
     
     return "patient data deleted"
-
-
